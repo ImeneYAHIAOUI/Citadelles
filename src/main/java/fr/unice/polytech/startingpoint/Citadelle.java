f426fed382a0bc4adabbba92537ddb8579ea6955
yvars <theo.yvars@gmail.com>
2021-11-11 19:36:18 +0100

    [DEV] Clean class

--- src/main/java/fr/unice/polytech/startingpoint/Citadelle.java
+++ src/main/java/fr/unice/polytech/startingpoint/Citadelle.java
@@ -2,12 +2,19 @@ package fr.unice.polytech.startingpoint;
 
 import fr.unice.polytech.startingpoint.cards.Color;
 import fr.unice.polytech.startingpoint.cards.District;
+import fr.unice.polytech.startingpoint.cards.DistrictDeck;
+import fr.unice.polytech.startingpoint.core.Initialization;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Citadelle {
+
+    /**
+     * Main method of the game
+     */
     public void game(){
+        DistrictDeck districtDeck = new DistrictDeck(Initialization.districtList());
 
     }
 }
