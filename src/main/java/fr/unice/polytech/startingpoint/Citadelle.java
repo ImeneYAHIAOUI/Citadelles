3949a81d4d4eb82d87ed5c8a8a21e3cbc84f5407
yvars <theo.yvars@gmail.com>
2021-11-11 19:32:50 +0100

    [DEV] Clean class

--- src/main/java/fr/unice/polytech/startingpoint/Citadelle.java
+++ src/main/java/fr/unice/polytech/startingpoint/Citadelle.java
@@ -1,4 +1,4 @@
-package proposition1;
+package fr.unice.polytech.startingpoint;
 
 import fr.unice.polytech.startingpoint.cards.Color;
 import fr.unice.polytech.startingpoint.cards.District;
@@ -7,53 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Citadelle {
-    public static void main(String args[]){
-        District district1 = new District(2,Color.JAUNE,"Cat1");
-        District district2 = new District(1,Color.ROUGE,"Cat2");
+    public void game(){
 
-        // YELLOW
-        for(int i = 0; i < 5; i++)
-            districtDeck.add(new District(1, Color.YELLOW,"Manoir"));
-        for(int i = 0; i < 4; i++)
-            districtDeck.add(new District(2,Color.YELLOW,"Château"));
-        for(int i = 0; i < 2; i++)
-            districtDeck.add(new District(2,Color.YELLOW,"Palais"));
-
-        Wonder wonder = new Wonder(4,Color.JAUNE,"Cat3","Something");
-
-        List<IDistrict> deck = new ArrayList<IDistrict>();
-        deck.add(district1);
-        deck.add(district2);
-        deck.add(wonder);
-
-        deck.forEach(district -> {
-            System.out.println(district.isWonder());
-            System.out.println(district.getColor());
-            System.out.println(district.getVal());
-
-            if(district.isWonder()){
-                ((IWonder)district).doAction();
-                ((IWonder)district).effectOfAction();
-            }
-        });
-
-
-            // INIT
-        Compare compare = new Compare();
-        Display display = new Display();
-        GameResult gameResult = new GameResult();
-
-
-            // Automate
-        /*
-        .
-        .
-        .
-        .
-         */
-        gameResult = compare.getResult(List IA);
-        display(gameResult);
-
-        ia.getDistrictList(districtDeck.giveDistrict(1));
     }
 }
--- src/main/java/fr/unice/polytech/startingpoint/Main.java
+++ src/main/java/fr/unice/polytech/startingpoint/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     public static void main(String... args) {
         System.out.println(hello());
+
+        Citadelle citadelle = new Citadelle();
+        citadelle.game();
     }
 
 }
