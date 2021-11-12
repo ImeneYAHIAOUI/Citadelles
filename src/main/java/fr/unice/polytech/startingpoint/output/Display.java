7e42c2ed835377dd37c9654dab5a8c48a7c8708f
ImeneYAHIAOUI <imene.yahiaoui@etu.univ-cotedazur.fr>
2021-11-12 20:16:39 +0100

    Display edited

--- src/main/java/fr/unice/polytech/startingpoint/output/Display.java
+++ src/main/java/fr/unice/polytech/startingpoint/output/Display.java
@@ -3,34 +3,60 @@ package fr.unice.polytech.startingpoint.output;
 import fr.unice.polytech.startingpoint.player.Player;
 
 import java.util.*;
+import java.util.stream.*;
+
 
 public class Display {
 
 
     private GameResult result;
 
-
     public Display(GameResult result) {
         this.result = result;
+
     }
-    public String displayResult(){
 
-        List<Player> ranking = result.getRanking();
-
-        String display = "Winner : " + ranking.get(0)+"\n\n";
-
-        int i = 1;
-        for (Player p: ranking) {
-            String s = switch (i){
+    public String displayRank(List<Player> players) {
+        String ranking;
+        int rank = 1;
+        ranking = "1st place : " + players.get(0) + " -> " + players.get(0).getScore() + " points.\n";
+        for (int i = 1; i < players.size(); i++) {
+            String s = switch (rank) {
                 case 1 -> "st";
                 case 2 -> "nd";
                 case 3 -> "rd";
                 default -> "th";
             };
-            display += i+s+" place : "+p+" -> "+p.getScore()+" points.\n";
-            i++;
+            ranking += rank + s + " place : " + players.get(i) + " -> " + players.get(i).getScore() + " points.\n";
+
+            if (players.get(i).getScore() != players.get(i - 1).getScore()) rank = i + 1;
+
+        }
+        return ranking;
+    }
+
+
+    public String displayWinners(List<Player> ranking){
+        String winners = ""+ranking.get(0) ;
+        int nbWinners = 1;
+
+        for(int i = 1; i < ranking.size();i++){
+            if (ranking.get(i).getScore() == ranking.get(0).getScore()) {
+                winners += " - "+ranking.get(i);
+                nbWinners++;
             }
-        return display;
+        }
+        String plural = nbWinners>1 ? "s":"";
+        winners = "Winner"+plural+" : "+winners+"\n\n";
+        return winners;
+    }
+
+    public String displayResult(){
+
+        List<Player> rankedPlayers = result.getRanking();
+
+
+        return displayWinners(rankedPlayers)+displayRank(rankedPlayers);
     }
 
 }
