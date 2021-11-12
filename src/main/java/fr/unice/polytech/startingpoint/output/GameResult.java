dd6fe44bb2ee14c12ed3d60cc4d3e18d443b2b74
baidouri <alibaidouri7@gmail.com>
2021-11-12 10:03:47 +0100

    GameResult

--- src/main/java/fr/unice/polytech/startingpoint/output/GameResult.java
+++ src/main/java/fr/unice/polytech/startingpoint/output/GameResult.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.startingpoint.output;
 
+import fr.unice.polytech.startingpoint.core.comparator;
 import fr.unice.polytech.startingpoint.player.Player;
 
 import java.util.ArrayList;
@@ -7,7 +8,8 @@ import java.util.ArrayList;
 public class GameResult {
 
 
-    private ArrayList<Player> players;
+    private ArrayList<Player> players = comparator.getPlayers();
+
 
     ArrayList<Player> getresult(){
          return  players  ;
