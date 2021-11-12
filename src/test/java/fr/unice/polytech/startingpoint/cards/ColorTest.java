66266fd521112d97f3428e935d7df31d4dbb96eb
yvars <theo.yvars@gmail.com>
2021-11-12 15:22:16 +0100

    [DEV] Add test

--- src/test/java/fr/unice/polytech/startingpoint/cards/ColorTest.java
+++ src/test/java/fr/unice/polytech/startingpoint/cards/ColorTest.java
@@ -1,7 +1,36 @@
 package fr.unice.polytech.startingpoint.cards;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColorTest {
+    private Color color;
 
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    /**
+     * Test the different possible colors of the cards
+     */
+    @Test
+    void testColor(){
+        this.color = Color.BLUE;
+        assertEquals(Color.BLUE, color);
+
+        this.color = Color.YELLOW;
+        assertEquals(Color.YELLOW, color);
+
+        this.color = Color.GREEN;
+        assertEquals(Color.GREEN, color);
+
+        this.color = Color.RED;
+        assertEquals(Color.RED, color);
+
+        this.color = Color.PURPLE;
+        assertEquals(Color.PURPLE, color);
+    }
 }
