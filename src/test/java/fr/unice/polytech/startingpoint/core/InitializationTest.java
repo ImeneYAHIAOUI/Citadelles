62d7eab532e819ebc7d93e79d5514a84187f7a8e
yvars <theo.yvars@gmail.com>
2021-11-13 11:15:53 +0100

    [DEV] Add Initiazation Test + Shuffle in Initialization #3

--- src/test/java/fr/unice/polytech/startingpoint/core/InitializationTest.java
+++ src/test/java/fr/unice/polytech/startingpoint/core/InitializationTest.java
@@ -1,7 +1,30 @@
 package fr.unice.polytech.startingpoint.core;
 
+import fr.unice.polytech.startingpoint.cards.District;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InitializationTest {
+    List<District> listTest = null;
+    int length = 0;
 
+    @BeforeEach
+    void setUp() {
+        this.listTest = new ArrayList<District>();
+        this.length = 11;
+    }
+
+    /**
+     * Test the length of the returned district list
+     */
+    @Test
+    void testDistrictList(){
+        this.listTest = Initialization.districtList();
+        assertEquals(this.listTest.size(), this.length);
+    }
 }
