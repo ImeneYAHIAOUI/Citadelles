2c7255eb8c1fcda2b122230722a7fb3cf4ad9838
sourour9 <sourour.gazzeh@outlook.fr>
2021-11-28 21:05:03 +0100

    ajout de la merveille bibliotheque #38

---
+++ src/main/java/fr/unice/polytech/startingpoint/cards/Library.java
@@ -0,0 +1,36 @@
+package fr.unice.polytech.startingpoint.cards;
+
+import java.util.List;
+
+public class Library extends DistrictD implements IWonder {
+    private String description;
+
+    public Library() {
+        this.name = DistrictName.LIBRARY;
+        this.color = Color.PURPLE;
+        this.price = 6;
+        this.description = "Si vous choisissez de" +
+                "piocher des cartes au " +
+                "début de votre tour," +
+                "vous en piochez deux " +
+                "et les conservez " +
+                "toutes les deux.\n";
+    }
+    @Override
+    public void doAction(infoaction info) {
+    }
+    @Override
+    public void effectOfAction() {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public boolean isWonder() {
+        return true ;
+    }
+    }
