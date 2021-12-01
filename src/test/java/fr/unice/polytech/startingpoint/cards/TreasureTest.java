8261d48ac6f96e8454809ac1f9a106a47b7540d3
sourour9 <sourour.gazzeh@outlook.fr>
2021-12-01 12:56:34 +0100

    classe test treasure #37

---
+++ src/test/java/fr/unice/polytech/startingpoint/cards/TreasureTest.java
@@ -0,0 +1,39 @@
+package fr.unice.polytech.startingpoint.cards;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TreasureTest {
+    Treasure treasure;
+    Treasure treasure1;
+
+
+
+    @BeforeEach
+    void setUp() {
+        treasure = new Treasure(32);
+        treasure1 = new Treasure(30);
+    }
+
+    @Test
+    void getPiecesTest(){
+        assertEquals(32,treasure.getPieces());
+        assertEquals(30,treasure1.getPieces());
+    }
+    @Test
+    void removeGoldTest(){
+        treasure.removeGold(2);
+        assertEquals(treasure1.getPieces(),treasure.getPieces());
+        assertEquals(30,treasure.getPieces());
+
+    }
+    @Test
+    void addToTreasureTest(){
+        treasure1.addToTreasure(2);
+        assertEquals(treasure1.getPieces(),treasure.getPieces());
+        assertEquals(32,treasure1.getPieces());
+    }
+
+}
