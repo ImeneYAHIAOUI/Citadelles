e76e38d982f41f4e6364fc31121b46ab3cca2452
sourour9 <sourour.gazzeh@outlook.fr>
2021-12-05 19:01:28 +0100

    Donjon #27

--- src/main/java/fr/unice/polytech/startingpoint/cards/DistrictName.java
+++ src/main/java/fr/unice/polytech/startingpoint/cards/DistrictName.java
@@ -25,6 +25,7 @@ public enum DistrictName {
         TOURDEGUET,
         PRISON,
         CASERNE,
-        FORTERESSE;
+        FORTERESSE,
+        DONGEON;
 
 }
---
+++ src/main/java/fr/unice/polytech/startingpoint/cards/Dongeon.java
@@ -0,0 +1,34 @@
+package fr.unice.polytech.startingpoint.cards;
+
+import java.util.List;
+
+public class Dongeon  extends DistrictD implements IWonder{
+    private String description;
+
+    public Dongeon() {
+        this.name = DistrictName.DONGEON;
+        this.color = Color.PURPLE;
+        this.price = 3;
+        this.description = "Le Donjon ne peut pas " +
+                "être détruit par le " +
+                "Condottière.\n";
+    }
+
+    @Override
+    public void doAction(infoaction info) {
+    }
+    @Override
+    public void effectOfAction() {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+
+    @Override
+    public boolean isWonder() {
+        return true ;
+    }
+}
