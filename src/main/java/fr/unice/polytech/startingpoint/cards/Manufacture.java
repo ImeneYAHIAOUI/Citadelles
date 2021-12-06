2f4bb0a430b6dee0cf7664135ddcfb2a53f016db
sourour9 <sourour.gazzeh@outlook.fr>
2021-12-06 13:30:02 +0100

    do action de la merveille manufacture 56

--- src/main/java/fr/unice/polytech/startingpoint/cards/Manufacture.java
+++ src/main/java/fr/unice/polytech/startingpoint/cards/Manufacture.java
@@ -23,11 +23,13 @@ public class Manufacture extends DistrictD implements IWonder {
     /** action of wonder as mentioned in "description"*/
     @Override
     public void doAction(infoaction info) {
-        IPlayer player=info.getplayer();
-        player.getDistrict(info.getattributeHand());
-        player.removeGold(3);
-        info.getTreasure().addToTreasure(3);}
-
+        if (info.getThreeManufacture()) {
+            IPlayer player = info.getplayer();
+            player.getDistrict(info.getattributeHand());
+            player.removeGold(3);
+            info.getTreasure().addToTreasure(3);
+        }
+    }
 
 
 
