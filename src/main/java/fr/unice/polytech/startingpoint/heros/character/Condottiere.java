3d43dd03db3e206da7c5dbbd07a19ce823a42552
sourour9 <sourour.gazzeh@outlook.fr>
2021-12-10 23:37:19 +0100

    ajout de la classe Condottiere  #71

---
+++ src/main/java/fr/unice/polytech/startingpoint/heros/character/Condottiere.java
@@ -0,0 +1,40 @@
+package fr.unice.polytech.startingpoint.heros.character;
+
+import fr.unice.polytech.startingpoint.cards.Color;
+import fr.unice.polytech.startingpoint.cards.IDistrict;
+import fr.unice.polytech.startingpoint.core.Treasure;
+import fr.unice.polytech.startingpoint.heros.Hero;
+import fr.unice.polytech.startingpoint.heros.HeroName;
+import fr.unice.polytech.startingpoint.player.IA.IAToHero;
+import fr.unice.polytech.startingpoint.player.IPlayer;
+
+public class Condottiere extends Hero {
+    public Condottiere() {
+        this.name = HeroName.Condottiere;
+        this.color = Color.RED;
+        this.rank = 8;
+    }
+
+    @Override
+    public void doAction(IAToHero information){
+        Treasure treasure=information.getTreasure();
+        IPlayer player=information.getCurrentPlayer();
+        IDistrict card=player.getCardDestroyedByCondottiere();
+        if(card!=null){
+            treasure.addToTreasure(card.getPrice()-1);
+            player.removeGold(card.getPrice()-1);
+            information.getDeck().getDistrictList().add(card);
+            information.getChosenPlayer().getBuiltDistricts().remove(card);
+            player.setCardDestroyedByCondottiere(null);
+        }
+        player.getBuiltDistricts().forEach(district -> {
+            if (district.getColor() == this.color) {
+                player.addGold(treasure.removeGold(1));
+            }
+        });
+
+
+
+
+    }
+}
---
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/Strategies/CondottiereChoice.java
@@ -0,0 +1,37 @@
+package fr.unice.polytech.startingpoint.player.IA.Strategies;
+
+import fr.unice.polytech.startingpoint.cards.DistrictD;
+import fr.unice.polytech.startingpoint.cards.DistrictName;
+import fr.unice.polytech.startingpoint.cards.IDistrict;
+import fr.unice.polytech.startingpoint.player.IA.IAToHero;
+import fr.unice.polytech.startingpoint.player.IPlayer;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class CondottiereChoice {
+
+    public void makeChoice(IAToHero information){
+        IPlayer currentPlayer=information.getCurrentPlayer();
+        int gold=currentPlayer.getGold();
+        List<Integer> scores=information.getScores();
+        int index=-1;
+        for (int score:scores) {
+            if(score>currentPlayer.getScore()){
+                if(information.getBuiltDistricts().get(scores.indexOf(score)).size()>=6){
+                    index=scores.indexOf(score);
+                    break;
+                }
+            }
+        }
+        if(index>0){
+            IDistrict district =information.getBuiltDistricts().get(index).stream().filter(card->card.getPrice()<=gold && card.getDistrictName()!= DistrictName.DONGEON).findAny().get();
+            currentPlayer.setCardDestroyedByCondottiere(district);
+            information.setChosenPlayer(information.getPlayersName().get(index));
+        }
+    }
+}
