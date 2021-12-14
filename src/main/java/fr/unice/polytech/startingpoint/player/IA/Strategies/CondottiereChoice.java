0c6eda3f6c91fe9e2c469c35efe87f98ebdda5ab
sourour9 <sourour.gazzeh@outlook.fr>
2021-12-14 19:35:53 +0100

    le choix du conditiere #71

--- src/main/java/fr/unice/polytech/startingpoint/player/IA/Strategies/CondottiereChoice.java
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/Strategies/CondottiereChoice.java
@@ -5,25 +5,43 @@ import fr.unice.polytech.startingpoint.cards.IDistrict;
 import fr.unice.polytech.startingpoint.player.IA.IAToHero;
 import fr.unice.polytech.startingpoint.player.IPlayer;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CondottiereChoice {
 
-    public void makeChoice(IAToHero information){
-        IPlayer currentPlayer=information.getCurrentPlayer();
-        int gold=currentPlayer.getGold();
-        List<Integer> scores=information.getScores();
-        int index=-1;
-        for (int score:scores){
-            if(score>currentPlayer.getScore()&& information.getBuiltDistricts().get(scores.indexOf(score)).size()>=6){
-                index=scores.indexOf(score);
-                break;
+    public void  makeChoice(IAToHero information) {
+        IPlayer currentPlayer = information.getCurrentPlayer();
+        int gold = currentPlayer.getGold();
+        List<Integer> scores = information.getScores();
+        int index;
+        IDistrict district = null;
+        for (int score : scores) {
+            index = scores.indexOf(score);
+            if (score > currentPlayer.getScore() && information.getBuiltDistricts().get(index).size() >= 6) {
+                district = information.getBuiltDistricts().get(index).stream()
+                        .filter(card -> card.getPrice() <= gold && card.getDistrictName() != DistrictName.DONGEON)
+                        .findAny().orElse(null);
+                if (district != null) {
+                    currentPlayer.setCardDestroyedByCondottiere(district);
+                    information.setChosenPlayer(information.getPlayersName().get(index));
+                    break;
+                }
             }
         }
-        if(index>=0){
-            IDistrict district =information.getBuiltDistricts().get(index).stream().filter(card->card.getPrice()<=gold && card.getDistrictName()!= DistrictName.DONGEON).findAny().orElse(null);
-            if(district!=null){
-                currentPlayer.setCardDestroyedByCondottiere(district);
-                information.setChosenPlayer(information.getPlayersName().get(index));            }
+        //on choisit de détruire un quartier à une seule piece
+        if (district == null) {
+            for (List<IDistrict> districts : information.getBuiltDistricts()) {
+                index = information.getBuiltDistricts().indexOf(districts);
+                district = districts.stream()
+                        .filter(card -> card.getPrice() == 1)
+                        .findAny().orElse(null);
+                if (district != null) {
+                    currentPlayer.setCardDestroyedByCondottiere(district);
+                    information.setChosenPlayer(information.getPlayersName().get(index));
+                    break;
+                }
+
+            }
         }
     }
 }
