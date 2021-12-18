5dc9a2c136f6abe87022a7fc50c468b3da732cc0
ImeneYAHIAOUI <imene.yahiaoui@etu.univ-cotedazur.fr>
2021-12-18 15:41:20 +0100

    add IA import in ThiefChoice #83

--- src/main/java/fr/unice/polytech/startingpoint/player/IA/Strategies/ThiefChoice.java
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/Strategies/ThiefChoice.java
@@ -4,6 +4,7 @@ import fr.unice.polytech.startingpoint.cards.IDistrict;
 import fr.unice.polytech.startingpoint.heros.HeroName;
 import fr.unice.polytech.startingpoint.heros.IHero;
 import fr.unice.polytech.startingpoint.player.IA.Bots;
+import fr.unice.polytech.startingpoint.player.IA.IA;
 import fr.unice.polytech.startingpoint.player.IPlayer;
 import fr.unice.polytech.startingpoint.player.IA.IAToHero;
 
