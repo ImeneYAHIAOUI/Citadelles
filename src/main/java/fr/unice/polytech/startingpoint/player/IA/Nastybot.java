c272b08d134bac41c620d20d7c04526915627788
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2021-12-18 14:04:16 +0100

    Condotier Choise #84

--- src/main/java/fr/unice/polytech/startingpoint/player/IA/Nastybot.java
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/Nastybot.java
@@ -1,4 +1,13 @@
 package fr.unice.polytech.startingpoint.player.IA;
 
-public class Nastybot {
+public class Nastybot extends IA {
+    /**
+     * @param playerName the IA object is constructed the same way as a Player object,
+     *                   so we also only need the name of the player here.
+     */
+    public Nastybot(String playerName){
+        super(playerName);
+        bot = Bots.nonSpecified;
+        niceNastyStrategy = NiceNastyBot.NASTY_BOT;
+    }
 }
--- src/main/java/fr/unice/polytech/startingpoint/player/IA/NiceBot.java
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/NiceBot.java
@@ -1,4 +1,13 @@
 package fr.unice.polytech.startingpoint.player.IA;
 
-public class NiceBot {
+public class NiceBot extends IA{
+    /**
+     * @param playerName the IA object is constructed the same way as a Player object,
+     *                   so we also only need the name of the player here.
+     */
+    public NiceBot(String playerName) {
+        super(playerName);
+        bot = Bots.nonSpecified;
+        niceNastyStrategy = NiceNastyBot.NICE_BOT;
+    }
 }
