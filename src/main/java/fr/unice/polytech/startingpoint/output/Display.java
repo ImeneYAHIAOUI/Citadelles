788e9c9ad66d0b07a466790910c37e42dd26e061
ImeneYAHIAOUI <imene.yahiaoui@etu.univ-cotedazur.fr>
2021-12-18 13:51:41 +0100

    add random hero choices #83

--- src/main/java/fr/unice/polytech/startingpoint/output/Display.java
+++ src/main/java/fr/unice/polytech/startingpoint/output/Display.java
@@ -13,7 +13,6 @@ import static fr.unice.polytech.startingpoint.cards.Color.*;
 
 public abstract class Display {
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
@@ -449,4 +448,5 @@ public abstract class Display {
         displayColorDistrictBonus(players);
         System.out.println();
     }
+
 }
