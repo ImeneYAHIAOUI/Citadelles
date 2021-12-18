a45de9b9729f619110ef4c6721a14f88baf14497
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2021-12-18 14:50:46 +0100

    BOT #84

---
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/NeutralBot.java
@@ -0,0 +1,13 @@
+package fr.unice.polytech.startingpoint.player.IA;
+
+public class NeutralBot extends IA{
+    /**
+     * @param playerName the IA object is constructed the same way as a Player object,
+     *                   so we also only need the name of the player here.
+     */
+    public NeutralBot(String playerName) {
+        super(playerName);
+        bot = Bots.nonSpecified;
+        niceNastyStrategy = NiceNastyBot.NEUTRAL_BOT;
+    }
+}
