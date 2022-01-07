10148d01c86fe6225af6ef75790f1ea4008f756d
Theophile-Yvars <79571637+Theophile-Yvars@users.noreply.github.com>
2022-01-07 17:36:47 +0100

    [DOC] LOGs #101

--- src/main/java/fr/unice/polytech/startingpoint/player/IA/BOTs/BuilderBot.java
+++ src/main/java/fr/unice/polytech/startingpoint/player/IA/BOTs/BuilderBot.java
@@ -3,6 +3,8 @@ package fr.unice.polytech.startingpoint.player.IA.BOTs;
 import fr.unice.polytech.startingpoint.player.IA.IA;
 import fr.unice.polytech.startingpoint.player.IA.StrategyBot;
 
+// BOT de la version FINAL
+
 public class BuilderBot extends IA {
     /**
      * @param playerName the IA object is constructed the same way as a Player object,
